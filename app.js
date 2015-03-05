22e07b796b1009c0c67d7f0aae618956481522ea
azkarproject <azkartest@gmail.com>
2015-03-05 13:19:28 +0100

    modif variable d'environnement

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 
 
 // Add pour 1-openshift 2-heroku 3-local
-ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.IP ||"127.0.0.1";
+ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.IPWEBSOCKETAZKAR ||"127.0.0.1";
 port      = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
 
 
