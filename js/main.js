57d3295f8978ee291a5ba7d0c827daa635356e84
azkarproject <azkartest@gmail.com>
2015-03-31 18:38:52 +0200

    Investigation BUG sdp offer + add coté serveur liste connectés

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ console.log(commonTest + " correctement chargé coté client !!!");
 // Fonctions websocket générales -----------------------------
 
 // Initialisation du canal de signalisation
-// Initialize shared signaling channel
 var socket = io.connect();
 
 
@@ -19,6 +18,7 @@ var socket = io.connect();
 // Si elle est déjà remplie, on ne fait rien...
 // Et comme ca on peut alléger les messages de la console
 socket.on('infoServer', function(data) {
+    console.log(">> socket.on('infoServer', function(data)");
     console.log(" Infos serveur: " + data);
 })
 
@@ -33,12 +33,17 @@ document.title = pseudo + ' - ' + document.title;
 
 // Quand on reçoit un message, on l'insère dans la page
 socket.on('message', function(data) {
+    console.log(">> socket.on('message', function(data)");
     insereMessage(data.pseudo, data.message)
-
 })
+
+
+var otherPeerPseudo = "???";
 
 // Quand un nouveau client se connecte, on affiche l'information
 socket.on('nouveau_client', function(pseudo) {
+    console.log(">> socket.on('nouveau_client', function(pseudo)");
+    // otherPeerPseudo = pseudo; 
     $('#zone_chat').prepend('<p><em>' + pseudo + ' a rejoint le Chat !</em></p>');
 })
 
@@ -63,7 +68,8 @@ navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
 
 
 // Déclaration des constraints pour la vidéo
-var constraint = {
+// TODO: Les constraintzs ne semblent pas prises en compte par Mozzilla
+var constraints = {
   audio:true,
   video: {
     mandatory: {
@@ -75,21 +81,20 @@ var constraint = {
 
 
 // On récupère le flux audio et vidéo local grace a getUserMedia
-// Acquire local audio and video streams
-//navigator.getUserMedia({ "audio": true, "video": true }, gotStream, logError);
-navigator.getUserMedia(constraint, gotStream, logError);
+navigator.getUserMedia(constraints, gotStream, logError);
 
 // Callback en cas d'echec de getUserMedia()
 function logError(error) {
+      console.log("@ logError(error)");
       console.log("getUserMedia error: " + error);
 };
 
 
 // Callback en cas de succès de getUserMedia()
 function gotStream(evt) {
-    
-    // Contrôle de l'objet envoyé ds addStream
-    // common.traceObjectDump(evt,'evt'); // Débugg
+    console.log("@ pc.gotStream(evt)");
+    // Contrôle de l'objet envoyé dans addStream
+    // common.traceObjectDump(evt,'evt'); // Débugg > OK
 
     // Register local MediaStream with peer connection
     // On ajoute le stream local à la connexion
@@ -100,20 +105,65 @@ function gotStream(evt) {
     var local_video = document.getElementById('localVideo');
     local_video.src = window.URL.createObjectURL(evt);
 
+    
+    // A n'executer que par l'apellant ??? Pas si sur...
+    // ------------------------------------------------------
     // On génère une offre SDP décrivant la connexion du pair 
     // et on l'envoie a l'autre pair.
     // Generate SDP offer describing peer connection and send to peer
+    
+    
+    // Import from codelab7 pour comprendre ce qui vas pas dans la construction du sdp
+    pc.createOffer(setLocalAndSendMessage, null, constraints);
+
+
+    /**/
+
+
+
+    /*//pc.setLocalDescription(offer);
     pc.createOffer(function(offer) {
-            pc.setLocalDescription(offer);
-            // signalingChannel.send(offer.sdp);
-            socket.emit('offer',offer.sdp);
+            console.log( ">> "+pseudo+" envoie une offre !!!");
+            //pc.setLocalDescription(offer);
+            socket.emit('offer',offer.sdp); // Bug ???
+            //socket.emit('offer',offer);
     });
+    /**/
+
+    /*
+    pc.createOffer(function(evt) {
+            console.log( ">> "+pseudo+" envoie une offre!!!");
+            pc.setLocalDescription(evt);
+            // signalingChannel.send(offer.sdp);
+            socket.emit('offer',evt.sdp);
+    });
+    /**/
+
+
 }
+
+// Ajout from codelab7...
+function setLocalAndSendMessage(sessionDescription) {
+  // Set Opus as the preferred codec in SDP if Opus is present.
+  // M.Buffa : la c'est de la tambouille compliquee pour modifier la 
+  // configuration SDP pour dire qu'on prefere un codec nomme OPUS (?)
+  // sessionDescription.sdp = preferOpus(sessionDescription.sdp);
+
+  pc.setLocalDescription(sessionDescription);
+
+  // Envoi par WebSocket
+  //sendMessage(sessionDescription);
+  console.log( ">> "+pseudo+" envoie une offre!!!");
+  socket.emit('offer',sessionDescription); // Bug ???
+}
+
+
 
 
 // Output remote video stream to video element (remote view)
 // On envoie la vidéo distante au document html
 pc.onaddstream = function (evt) {
+    console.log("@ pc.onaddstream(evt)");
     var remote_video = document.getElementById('remoteVideo');
     remote_video.src = window.URL.createObjectURL(evt);
 }
@@ -125,11 +175,13 @@ pc.onaddstream = function (evt) {
 
 // Trickle ICE candidates to the peer via the signaling channel
 pc.onicecandidate = function(evt) {
+    console.log("@ pc.onicecandidate(evt)");
     // common.traceObjectDump(evt,'onicecandidate(evt)'); // Débugg
     if (evt.candidate) {
       // signalingChannel.send(evt.candidate);
       // socket.send(evt.candidate);
-      socket.emit('candidate',evt.candidate);
+      socket.emit('candidate',evt.candidate); // bug ???
+      //socket.emit('candidate',evt);
     }
 }
 
@@ -138,21 +190,30 @@ pc.onicecandidate = function(evt) {
 // On enregistre l'ICE candidate por commencer tests de connexion
 // Register remote ICE candidate to begin connectivity checks
 socket.on('candidate', function(msg) { 
+   console.log(">> socket.on('candidate', function(msg)");
    if (msg.candidate) {
-      common.traceObjectDump(msg,'socket.on(candidate)'); // Débugg
-      pc.addIceCandidate(msg.candidate);
+      // common.traceObjectDump(msg,'socket.on(candidate)'); // Débugg
+      pc.addIceCandidate(msg.candidate); // Bug !
+      // Message d'erreur >>>>
+      // Uncaught TypeMismatchError: Failed to execute 'saddIceCandidate' on 'RTCPeerConnection': 
+      // The 1st argument provided is either null, or an invalid RTCSessionDescription object.
+      // -----------------------
    }
 })
 
 
-
 // Quand on reçoit un message "offer"
-socket.on('createOffer', function(msg) { 
-    pc.setRemoteDescription(msg.offer);
+socket.on('offer', function(data) { 
+    console.log( ">> "+pseudo+" recoie une offre de " + data.pseudo);
+    common.AlertObjectDump(data.message,'socket.on(offer) > data.message'); // Débugg
+    pc.setRemoteDescription(data.message); // ??
+    // pc.setRemoteDescription(data.message.offer); // BUG
+    // Message d'erreur >>>>
+    // Uncaught TypeMismatchError: Failed to execute 'setRemoteDescription' on 'RTCPeerConnection': 
+    // The 1st argument provided is either null, or an invalid RTCSessionDescription object.
+    // -----------------------
+    
 })
-
-
-
 
 // Fin Signaling par websocket --------------------------------------------
 
