39cb6d83bb1f79986d1264826a9c24ec82c67b37
KFMichael <kangahmichael@gmail.com>
2015-08-10 13:48:14 +0200

    modif server

--- js/lib/xml2json.js
+++ js/lib/xml2json.js
@@ -1,155 +1,171 @@
-/*	This work is licensed under Creative Commons GNU LGPL License.
+/*
+xml2json v 1.1
+copyright 2005-2007 Thomas Frank
 
-	License: http://creativecommons.org/licenses/LGPL/2.1/
-   Version: 0.9
-	Author:  Stefan Goessner/2006
-	Web:     http://goessner.net/ 
+This program is free software under the terms of the 
+GNU General Public License version 2 as published by the Free 
+Software Foundation. It is distributed without any warranty.
 */
-function xml2json(xml, tab) {
-   var X = {
-      toObj: function(xml) {
-         var o = {};
-         if (xml.nodeType==1) {   // element node ..
-            if (xml.attributes.length)   // element with attributes  ..
-               for (var i=0; i<xml.attributes.length; i++)
-                  o["@"+xml.attributes[i].nodeName] = (xml.attributes[i].nodeValue||"").toString();
-            if (xml.firstChild) { // element has child nodes ..
-               var textChild=0, cdataChild=0, hasElementChild=false;
-               for (var n=xml.firstChild; n; n=n.nextSibling) {
-                  if (n.nodeType==1) hasElementChild = true;
-                  else if (n.nodeType==3 && n.nodeValue.match(/[^ \f\n\r\t\v]/)) textChild++; // non-whitespace text
-                  else if (n.nodeType==4) cdataChild++; // cdata section node
-               }
-               if (hasElementChild) {
-                  if (textChild < 2 && cdataChild < 2) { // structured element with evtl. a single text or/and cdata node ..
-                     X.removeWhite(xml);
-                     for (var n=xml.firstChild; n; n=n.nextSibling) {
-                        if (n.nodeType == 3)  // text node
-                           o["#text"] = X.escape(n.nodeValue);
-                        else if (n.nodeType == 4)  // cdata node
-                           o["#cdata"] = X.escape(n.nodeValue);
-                        else if (o[n.nodeName]) {  // multiple occurence of element ..
-                           if (o[n.nodeName] instanceof Array)
-                              o[n.nodeName][o[n.nodeName].length] = X.toObj(n);
-                           else
-                              o[n.nodeName] = [o[n.nodeName], X.toObj(n)];
-                        }
-                        else  // first occurence of element..
-                           o[n.nodeName] = X.toObj(n);
-                     }
-                  }
-                  else { // mixed content
-                     if (!xml.attributes.length)
-                        o = X.escape(X.innerXml(xml));
-                     else
-                        o["#text"] = X.escape(X.innerXml(xml));
-                  }
-               }
-               else if (textChild) { // pure text
-                  if (!xml.attributes.length)
-                     o = X.escape(X.innerXml(xml));
-                  else
-                     o["#text"] = X.escape(X.innerXml(xml));
-               }
-               else if (cdataChild) { // cdata
-                  if (cdataChild > 1)
-                     o = X.escape(X.innerXml(xml));
-                  else
-                     for (var n=xml.firstChild; n; n=n.nextSibling)
-                        o["#cdata"] = X.escape(n.nodeValue);
-               }
+
+xml2json={
+   parser:function(xmlcode,ignoretags,debug){
+      if(!ignoretags){ignoretags=""};
+      xmlcode=xmlcode.replace(/\s*\/>/g,'/>');
+      xmlcode=xmlcode.replace(/<\?[^>]*>/g,"").replace(/<\![^>]*>/g,"");
+      if (!ignoretags.sort){ignoretags=ignoretags.split(",")};
+      var x=this.no_fast_endings(xmlcode);
+      x=this.attris_to_tags(x);
+      x=escape(x);
+      x=x.split("%3C").join("<").split("%3E").join(">").split("%3D").join("=").split("%22").join("\"");
+      for (var i=0;i<ignoretags.length;i++){
+         x=x.replace(new RegExp("<"+ignoretags[i]+">","g"),"*$**"+ignoretags[i]+"**$*");
+         x=x.replace(new RegExp("</"+ignoretags[i]+">","g"),"*$***"+ignoretags[i]+"**$*")
+      };
+      x='<JSONTAGWRAPPER>'+x+'</JSONTAGWRAPPER>';
+      this.xmlobject={};
+      var y=this.xml_to_object(x).jsontagwrapper;
+      if(debug){y=this.show_json_structure(y,debug)};
+      return y
+   },
+   xml_to_object:function(xmlcode){
+      var x=xmlcode.replace(/<\//g,"§");
+      x=x.split("<");
+      var y=[];
+      var level=0;
+      var opentags=[];
+      for (var i=1;i<x.length;i++){
+         var tagname=x[i].split(">")[0];
+         opentags.push(tagname);
+         level++
+         y.push(level+"<"+x[i].split("§")[0]);
+         while(x[i].indexOf("§"+opentags[opentags.length-1]+">")>=0){level--;opentags.pop()}
+      };
+      var oldniva=-1;
+      var objname="this.xmlobject";
+      for (var i=0;i<y.length;i++){
+         var preeval="";
+         var niva=y[i].split("<")[0];
+         var tagnamn=y[i].split("<")[1].split(">")[0];
+         tagnamn=tagnamn.toLowerCase();
+         var rest=y[i].split(">")[1];
+         if(niva<=oldniva){
+            var tabort=oldniva-niva+1;
+            for (var j=0;j<tabort;j++){objname=objname.substring(0,objname.lastIndexOf("."))}
+         };
+         objname+="."+tagnamn;
+         var pobject=objname.substring(0,objname.lastIndexOf("."));
+         if (eval("typeof "+pobject) != "object"){preeval+=pobject+"={value:"+pobject+"};\n"};
+         var objlast=objname.substring(objname.lastIndexOf(".")+1);
+         var already=false;
+         for (k in eval(pobject)){if(k==objlast){already=true}};
+         var onlywhites=true;
+         for(var s=0;s<rest.length;s+=3){
+            if(rest.charAt(s)!="%"){onlywhites=false}
+         };
+         if (rest!="" && !onlywhites){
+            if(rest/1!=rest){
+               rest="'"+rest.replace(/\'/g,"\\'")+"'";
+               rest=rest.replace(/\*\$\*\*\*/g,"</");
+               rest=rest.replace(/\*\$\*\*/g,"<");
+               rest=rest.replace(/\*\*\$\*/g,">")
             }
-            if (!xml.attributes.length && !xml.firstChild) o = null;
+         } 
+         else {rest="{}"};
+         if(rest.charAt(0)=="'"){rest='unescape('+rest+')'};
+         if (already && !eval(objname+".sort")){preeval+=objname+"=["+objname+"];\n"};
+         var before="=";after="";
+         if (already){before=".push(";after=")"};
+         var toeval=preeval+objname+before+rest+after;
+         eval(toeval);
+         if(eval(objname+".sort")){objname+="["+eval(objname+".length-1")+"]"};
+         oldniva=niva
+      };
+      return this.xmlobject
+   },
+   show_json_structure:function(obj,debug,l){
+      var x='';
+      if (obj.sort){x+="[\n"} else {x+="{\n"};
+      for (var i in obj){
+         if (!obj.sort){x+=i+":"};
+         if (typeof obj[i] == "object"){
+            x+=this.show_json_structure(obj[i],false,1)
          }
-         else if (xml.nodeType==9) { // document.node
-            o = X.toObj(xml.documentElement);
-         }
-         else
-            alert("unhandled node type: " + xml.nodeType);
-         return o;
-      },
-      toJson: function(o, name, ind) {
-         var json = name ? ("\""+name+"\"") : "";
-         if (o instanceof Array) {
-            for (var i=0,n=o.length; i<n; i++)
-               o[i] = X.toJson(o[i], "", ind+"\t");
-            json += (name?":[":"[") + (o.length > 1 ? ("\n"+ind+"\t"+o.join(",\n"+ind+"\t")+"\n"+ind) : o.join("")) + "]";
-         }
-         else if (o == null)
-            json += (name&&":") + "null";
-         else if (typeof(o) == "object") {
-            var arr = [];
-            for (var m in o)
-               arr[arr.length] = X.toJson(o[m], m, ind+"\t");
-            json += (name?":{":"{") + (arr.length > 1 ? ("\n"+ind+"\t"+arr.join(",\n"+ind+"\t")+"\n"+ind) : arr.join("")) + "}";
-         }
-         else if (typeof(o) == "string")
-            json += (name&&":") + "\"" + o.toString() + "\"";
-         else
-            json += (name&&":") + o.toString();
-         return json;
-      },
-      innerXml: function(node) {
-         var s = ""
-         if ("innerHTML" in node)
-            s = node.innerHTML;
          else {
-            var asXml = function(n) {
-               var s = "";
-               if (n.nodeType == 1) {
-                  s += "<" + n.nodeName;
-                  for (var i=0; i<n.attributes.length;i++)
-                     s += " " + n.attributes[i].nodeName + "=\"" + (n.attributes[i].nodeValue||"").toString() + "\"";
-                  if (n.firstChild) {
-                     s += ">";
-                     for (var c=n.firstChild; c; c=c.nextSibling)
-                        s += asXml(c);
-                     s += "</"+n.nodeName+">";
-                  }
-                  else
-                     s += "/>";
-               }
-               else if (n.nodeType == 3)
-                  s += n.nodeValue;
-               else if (n.nodeType == 4)
-                  s += "<![CDATA[" + n.nodeValue + "]]>";
-               return s;
+            if(typeof obj[i]=="function"){
+               var v=obj[i]+"";
+               //v=v.replace(/\t/g,"");
+               x+=v
+            }
+            else if(typeof obj[i]!="string"){x+=obj[i]+",\n"}
+            else {x+="'"+obj[i].replace(/\'/g,"\\'").replace(/\n/g,"\\n").replace(/\t/g,"\\t").replace(/\r/g,"\\r")+"',\n"}
+         }
+      };
+      if (obj.sort){x+="],\n"} else {x+="},\n"};
+      if (!l){
+         x=x.substring(0,x.lastIndexOf(","));
+         x=x.replace(new RegExp(",\n}","g"),"\n}");
+         x=x.replace(new RegExp(",\n]","g"),"\n]");
+         var y=x.split("\n");x="";
+         var lvl=0;
+         for (var i=0;i<y.length;i++){
+            if(y[i].indexOf("}")>=0 || y[i].indexOf("]")>=0){lvl--};
+            tabs="";for(var j=0;j<lvl;j++){tabs+="\t"};
+            x+=tabs+y[i]+"\n";
+            if(y[i].indexOf("{")>=0 || y[i].indexOf("[")>=0){lvl++}
+         };
+         if(debug=="html"){
+            x=x.replace(/</g,"&lt;").replace(/>/g,"&gt;");
+            x=x.replace(/\n/g,"<BR>").replace(/\t/g,"&nbsp;&nbsp;&nbsp;&nbsp;")
+         };
+         if (debug=="compact"){x=x.replace(/\n/g,"").replace(/\t/g,"")}
+      };
+      return x
+   },
+   no_fast_endings:function(x){
+      x=x.split("/>");
+      for (var i=1;i<x.length;i++){
+         var t=x[i-1].substring(x[i-1].lastIndexOf("<")+1).split(" ")[0];
+         x[i]="></"+t+">"+x[i]
+      }  ;
+      x=x.join("");
+      return x
+   },
+   attris_to_tags: function(x){
+      var d=' ="\''.split("");
+      x=x.split(">");
+      for (var i=0;i<x.length;i++){
+         var temp=x[i].split("<");
+         for (var r=0;r<4;r++){temp[0]=temp[0].replace(new RegExp(d[r],"g"),"_jsonconvtemp"+r+"_")};
+         if(temp[1]){
+            temp[1]=temp[1].replace(/'/g,'"');
+            temp[1]=temp[1].split('"');
+            for (var j=1;j<temp[1].length;j+=2){
+               for (var r=0;r<4;r++){temp[1][j]=temp[1][j].replace(new RegExp(d[r],"g"),"_jsonconvtemp"+r+"_")}
             };
-            for (var c=node.firstChild; c; c=c.nextSibling)
-               s += asXml(c);
-         }
-         return s;
-      },
-      escape: function(txt) {
-         return txt.replace(/[\\]/g, "\\\\")
-                   .replace(/[\"]/g, '\\"')
-                   .replace(/[\n]/g, '\\n')
-                   .replace(/[\r]/g, '\\r');
-      },
-      removeWhite: function(e) {
-         e.normalize();
-         for (var n = e.firstChild; n; ) {
-            if (n.nodeType == 3) {  // text node
-               if (!n.nodeValue.match(/[^ \f\n\r\t\v]/)) { // pure whitespace text node
-                  var nxt = n.nextSibling;
-                  e.removeChild(n);
-                  n = nxt;
-               }
-               else
-                  n = n.nextSibling;
-            }
-            else if (n.nodeType == 1) {  // element node
-               X.removeWhite(n);
-               n = n.nextSibling;
-            }
-            else                      // any other node
-               n = n.nextSibling;
-         }
-         return e;
-      }
-   };
-   if (xml.nodeType == 9) // document node
-      xml = xml.documentElement;
-   var json = X.toJson(X.toObj(X.removeWhite(xml)), xml.nodeName, "\t");
-   return "{\n" + tab + (tab ? json.replace(/\t/g, tab) : json.replace(/\t|\n/g, "")) + "\n}";
-}
+            temp[1]=temp[1].join('"')
+         };
+         x[i]=temp.join("<")
+      };
+      x=x.join(">");
+      x=x.replace(/ ([^=]*)=([^ |>]*)/g,"><$1>$2</$1");
+      x=x.replace(/>"/g,">").replace(/"</g,"<");
+      for (var r=0;r<4;r++){x=x.replace(new RegExp("_jsonconvtemp"+r+"_","g"),d[r])}   ;
+      return x
+   }
+};
+
+
+if(!Array.prototype.push){
+   Array.prototype.push=function(x){
+      this[this.length]=x;
+      return true
+   }
+};
+
+if (!Array.prototype.pop){
+   Array.prototype.pop=function(){
+      var response = this[this.length-1];
+      this.length--;
+      return response
+   }
+};
