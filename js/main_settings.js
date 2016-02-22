c2e02878ad3a38f90643cde16f73336a0fb24764
azkarproject <azkartest@gmail.com>
2016-02-22 16:26:21 +0100

    rfc5766 tests

--- js/main_settings.js
+++ js/main_settings.js
@@ -172,8 +172,8 @@ navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || 
 server = {'iceServers': []}; // OK sur même réseau...
 //server.iceServers.push({ url: 'stun:stun.l.google.com:19302'});
 
-//server.iceServers.push({urls: "turn:134.59.130.142:3478",credential: TURN_credential ,username: TURN_username}); // rfc5766 sur VM2
-server.iceServers.push({urls: "turn:134.59.130.142:443",credential: TURN_credential ,username: TURN_username}); // rfc5766 sur VM2
+server.iceServers.push({urls: "turn:134.59.130.142:3478",credential: TURN_credential ,username: TURN_username}); // rfc5766 sur VM2
+//server.iceServers.push({urls: "turn:134.59.130.142:443",credential: TURN_credential ,username: TURN_username}); // rfc5766 sur VM2
 //server.iceServers.push({urls: "turn:134.59.130.142:80",credential: TURN_credential ,username: TURN_username}); // rfc5766 sur VM2
 
 // -------------------------------------------------------------------
