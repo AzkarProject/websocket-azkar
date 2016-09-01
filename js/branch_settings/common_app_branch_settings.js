3b49349e9237022c29de129f086f7b4192e1f06a
azkarproject <azkartest@gmail.com>
2016-09-01 19:00:54 +0200

    debug inversion robot/pilote si wifi unice

--- js/branch_settings/common_app_branch_settings.js
+++ js/branch_settings/common_app_branch_settings.js
@@ -169,7 +169,7 @@
 
 	  
 	exports.isFakeRobubox = function() {
-		var fakeRobubox = false;
+		var fakeRobubox = true;
 		return fakeRobubox;
 	}
 
@@ -178,8 +178,8 @@
 		// var mapSource = '/images/mapLaboI3S.png'; // Carte I3S Robubox V2
 		//var mapSource = '/images/labo3.png'; // Carte I3S Robubox V3
 		//var mapSource = '/images/hu0.png'; // Carte MGG
-		//var mapSource = '/images/labo261.png'; // Carte I3S Via Dashboard Mobiserv
-		var mapSource = '/images/labovilette.png'; // Carte CS La Vilette Via Dashboard Mobiserv
+		var mapSource = '/images/labo261.png'; // Carte I3S Via Dashboard Mobiserv
+		//var mapSource = '/images/labovilette.png'; // Carte CS La Vilette Via Dashboard Mobiserv
 		return mapSource
 	}
 
