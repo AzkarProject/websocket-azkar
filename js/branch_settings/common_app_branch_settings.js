adbe30313191004519e7c92789a52339d2aa137c
azkarproject <azkartest@gmail.com>
2016-09-08 17:16:16 +0200

    Fakerobubox activé

--- js/branch_settings/common_app_branch_settings.js
+++ js/branch_settings/common_app_branch_settings.js
@@ -169,7 +169,7 @@
 
 	  
 	exports.isFakeRobubox = function() {
-		var fakeRobubox = false;
+		var fakeRobubox = true;
 		return fakeRobubox;
 	}
 
@@ -178,11 +178,11 @@
 		// var mapSource = '/images/mapLaboI3S.png'; // Carte I3S Robubox V2
 		// var mapSource = '/images/labo3.png'; // Carte I3S Robubox V3
 		// var mapSource = '/images/hu0.png'; // Carte MGG
-		// var mapSource = '/images/labo261.png'; // Carte I3S Via Dashboard Mobiserv
+		var mapSource = '/images/labo261.png'; // Carte I3S Via Dashboard Mobiserv
 		// var mapSource = '/images/labovilette.png'; // Carte CS La Vilette Via Dashboard Mobiserv
 		// var mapSource = '/images/lavilette-salle-de-jeux.png'; // Carte CS La Vilette V2 Via Dashboard Mobiserv
 		// var mapSource = '/images/PenBron0.png'; // Carte PenBron: 
-		var mapSource = '/images/atelierlavilette30.png'; // Carte CS La Vilette V3 Via Dashboard Mobiserv
+		// var mapSource = '/images/atelierlavilette30.png'; // Carte CS La Vilette V3 Via Dashboard Mobiserv
 		return mapSource
 	}
 
