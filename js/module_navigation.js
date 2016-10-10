0ab4a0a7a85b7baf2d28e0384c80a72e3011fae3
azkarproject <azkartest@gmail.com>
2016-10-10 13:57:38 +0200

    Debugg loop ws recommandations

--- js/module_navigation.js
+++ js/module_navigation.js
@@ -631,6 +631,7 @@
                 }
 
             }
+            
             console.log("webSemanticRecommandations(nearestPoiName = "+nearestPoiName+")")
             console.log("webSemanticRecommandations(activeRecommandation = "+activeRecommandation+")")
             console.log("webSemanticRecommandations(isLinkedToWS = "+isLinkedToWS+")")
