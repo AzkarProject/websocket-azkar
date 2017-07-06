27e84d88a0c6e6d6d2a11ba556651d8a58058e09
azkarproject <azkartest@gmail.com>
2017-07-06 13:22:08 +0200

    IP VM1 & port 443 + Add map Blendmgg0

--- js/common_app_settings.js
+++ js/common_app_settings.js
@@ -67,8 +67,8 @@
 	exports.appServerIp = function(){
 		//return "localhost";
 		//return "192.168.173.1" // réseau AdHoc 'Robulab_wifi'
-		//return "134.59.130.143" // VM 1 Sparks
-		return "134.59.130.142" // VM 2 Sparks
+		return "134.59.130.143" // VM 1 Sparks
+		//return "134.59.130.142" // VM 2 Sparks
 	}
 
 	// N° de port: Idéalement 443 pour du https.
